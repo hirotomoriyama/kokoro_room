cb25d13e6007f2c9413e40d068ae5ae3fcfa5f86
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-09 13:25:18 +0000

    [Add]会員のホーム、アバウト、認証機能のコントローラー、ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: 'public' do
+
+    root to: "homes#top"
+
+    get "/about" => "homes#about"
+  end
+
+  devise_for :members, controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
+
 end
