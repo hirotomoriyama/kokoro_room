285e697f0372c224903abaea094ae18f2d98c0c7
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-11 03:35:10 +0000

    [Add]カテゴリーindex,createのアクション内定義、ヘッダー、フッターのコメントアウト設定

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,15 +1,28 @@
 class Admin::CategoriesController < Admin::ApplicationController
 
   def index
+    @category = Category.new
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.save
+    # 投稿後、同じページに遷移
+    redirect_back(fallback_location: root_path)
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  # カテゴリー投稿、編集時、カテゴリー名を入力するための設定
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
