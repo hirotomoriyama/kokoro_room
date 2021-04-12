c9e61e2d048af4032dd1024cea0871c7844f8a28
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-12 12:34:33 +0000

    [Add]悩み投稿画面の作成

--- app/controllers/public/problems_controller.rb
+++ app/controllers/public/problems_controller.rb
@@ -4,15 +4,27 @@ class Public::ProblemsController < Public::ApplicationController
   end
 
   def new
+    @problem = Problem.new
   end
 
   def create
+    @problem = Problem.new(problem_params)
+    @problem.save
+    # 悩み投稿後、トップページ（マイページ）へ遷移
+    redirect_to root_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  # 悩み投稿時、タイトルと本文を入力するための設定
+  def problem_params
+    params.require(:problem).permit(:title, :body)
   end
 
 end
