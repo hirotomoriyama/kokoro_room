b13b69bd69bb8022f3839b33bdb1fed4e5dc13ff
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-12 07:14:46 +0000

    [Add]memberコントローラーに退会処理の記述

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,9 +1,17 @@
 class Public::MembersController < Public::ApplicationController
 
   def unsubscribe
+    # 退会確認画面を開くのはログインユーザーのみ
+    @member = current_member
   end
 
+  # アカウント削除ではなく、論理削除（データ復旧可能にするため）
   def withdraw
+    @member = current_member
+    # falseが有効状態、trueが退会状態
+    @member.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
 end
