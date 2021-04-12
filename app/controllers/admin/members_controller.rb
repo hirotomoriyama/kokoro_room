940fac9327ca6cea9e94df73e82cc39ef6f7fb8a
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-12 10:05:58 +0000

    [Add]会員編集画面を作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -9,9 +9,21 @@ class Admin::MembersController < Admin::ApplicationController
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def update
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    # 編集後は会員詳細画面へ遷移
+    redirect_to admin_member_path
+  end
+
+  private
+
+  #会員ステータス編集時、選択するための設定
+  def member_params
+    params.require(:member).permit(:is_deleted)
   end
 
 end
