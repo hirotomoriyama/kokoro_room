e42c8b4ad8c99e48ced469c9b80dc158218c292e
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-16 14:59:00 +0000

    [Add]返事の投稿機能を作成

--- app/controllers/public/responses_controller.rb
+++ app/controllers/public/responses_controller.rb
@@ -1,12 +1,29 @@
 class Public::ResponsesController < Public::ApplicationController
 
   def new
+    @response = Response.new
+    @advice = Advice.find(params[:advice_id])
   end
 
   def create
+    response = Response.new(response_params)
+    # ログイン中の会員が返事を投稿
+    response.member_id = current_member.id
+    # 返事に対する回答の指定
+    response.advice_id = params[:advice_id]
+    response.save
+     # 返事投稿後、悩み事詳細画面に遷移
+    redirect_to problem_path(response.advice.problem_id)
   end
 
   def destroy
+  end
+
+  private
+
+  # 返事投稿時、返事の内容を投稿するための設定
+  def response_params
+    params.require(:response).permit(:body)
   end
 
 end
