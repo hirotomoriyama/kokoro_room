85af6d377df5a8d58dd18037ed8c2e9ea26538bf
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-17 14:19:50 +0000

    [Add]検索フォームをヘッダーに作成、searchコントローラーの記述

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,29 @@
 class Public::SearchesController < Public::ApplicationController
 
   def search
+    @model = params['search']['model']
+    @content = params['search']['content']
+    @how = params['search']['how']
+    @datas = search_for(@model, @content, @how)
+  end
+
+  private
+
+  def match(model, content)
+    Problem.where(title: content)
+  end
+
+  def partical(model, content)
+    Problem.where('title LIKE ?', "%#{content}%")
+  end
+
+  def search_for(model, content, how)
+    case how
+    when 'match'
+      match(model, content)
+    when 'partical'
+      partical(model, content)
+    end
   end
 
 end
