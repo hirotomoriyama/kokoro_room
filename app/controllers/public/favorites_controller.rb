ce78ff87915d0563e8ccc3139231a32615801ef1
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-04-24 06:16:46 +0000

    [Update]いいね機能に非同期通信を実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,19 @@
 class Public::FavoritesController < Public::ApplicationController
 
   def create
-    advice = Advice.find(params[:advice_id])
-    favorite = current_member.favorites.new(advice_id: advice.id)
+    @problem = Problem.find(params[:problem_id])
+    @advice = Advice.find(params[:advice_id])
+    # いいねを付ける回答の指定
+    favorite = current_member.favorites.new(advice_id: @advice.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    advice = Advice.find(params[:advice_id])
-    favorite = current_member.favorites.find_by(advice_id: advice.id)
+    @problem = Problem.find(params[:problem_id])
+    @advice = Advice.find(params[:advice_id])
+    # いいねを外す回答の指定
+    favorite = current_member.favorites.find_by(advice_id: @advice.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
