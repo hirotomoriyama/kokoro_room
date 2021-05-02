307775b6222515abbd0ebeba0c476b280de945bc
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-02 14:24:56 +0000

    [Add]お問い合わせ機能のモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_085838) do
+ActiveRecord::Schema.define(version: 2021_05_02_142303) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_04_24_085838) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
