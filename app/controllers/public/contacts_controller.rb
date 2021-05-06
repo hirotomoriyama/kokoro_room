c3dadefe80c32a381b3552d203795d2e08a1ae44
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-06 13:46:12 +0000

    [Add]問い合わせコントローラーのアクション内を定義

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,7 +1,28 @@
 class Public::ContactsController < Public::ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    # ログイン中の会員が投稿
+    @contact.member_id = current_member.id
+    if @contact.save
+      ContactMailer.contact_mail(@contact).deliver
+      flash[:notice] = '問い合わせ内容を送信しました'
+      # 問い合わせ内容投稿後、トップページ（マイページ）へ遷移
+      redirect_to root_path
+    else
+      flash.now[:alert] = '項目を入力してください'
+      # 問い合わせ内容投稿失敗後、問い合わせフォームへ遷移
+      render :new
+    end
+  end
+
+  private
+
+  # 問い合わせ内容の投稿時、名前と本文を入力するための設定
+  def contact_params
+    params.require(:contact).permit(:name, :body)
   end
 end
