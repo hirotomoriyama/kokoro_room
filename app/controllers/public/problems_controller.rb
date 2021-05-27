ee83d66436f985d23d829d735e02b5c99794e87c
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-27 13:56:21 +0000

    [Add]悩み事一覧画面に悩み事の絞り込み検索機能を追加

--- app/controllers/public/problems_controller.rb
+++ app/controllers/public/problems_controller.rb
@@ -1,6 +1,7 @@
 class Public::ProblemsController < Public::ApplicationController
   def index
-    @problems = Problem.all
+    option = params['option']
+    @problems = search_for(option)
   end
 
   def new
@@ -48,5 +49,27 @@ class Public::ProblemsController < Public::ApplicationController
   # 悩み投稿時、タイトルと本文を入力、カテゴリーを選択するための設定
   def problem_params
     params.require(:problem).permit(:title, :body, :category_id, :best_answer_id)
+  end
+
+  def search_for(option)
+    # 全て表示の場合
+    if option == "all"
+      Problem.all
+    # 回答受付中の場合
+    elsif option == "unsolved"
+      @members = Member.where(is_deleted: false)
+      Problem.where(best_answer_id: nil, member_id: @members.ids)
+    # 解決済みの場合
+    elsif option == "solved"
+      @members = Member.where(is_deleted: false)
+      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids)
+    # 回答受付終了の場合
+    elsif option == "finished"
+      @members = Member.where(is_deleted: true)
+      Problem.where(best_answer_id: nil, member_id: @members.ids)
+    # 未選択の場合
+    else
+      Problem.all
+    end
   end
 end
