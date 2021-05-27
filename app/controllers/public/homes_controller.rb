a6529ee589b5709cb8926afaac9ade9cdab3ab9e
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-27 10:49:17 +0000

    [Add]トップページに悩み事の絞り込み検索機能を追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,8 @@ class Public::HomesController < Public::ApplicationController
   def top
     if member_signed_in?
       @member = current_member
-      @problems = @member.problems
+      option = params['option']
+      @problems = search_for(option)
     end
   end
 
@@ -10,5 +11,24 @@ class Public::HomesController < Public::ApplicationController
   end
 
   def map
+  end
+
+  private
+
+  # 悩み事の絞り込み検索時の選択肢
+  def search_for(option)
+    # 全て表示の場合
+    if option == "all"
+      @member.problems
+    # 回答受付中の場合
+    elsif option == "unsolved"
+      @member.problems.where(best_answer_id: nil)
+    # 解決済みの場合
+    elsif option == "solved"
+      @member.problems.where.not(best_answer_id: nil)
+    # 未選択の場合
+    else
+      @member.problems
+    end
   end
 end
