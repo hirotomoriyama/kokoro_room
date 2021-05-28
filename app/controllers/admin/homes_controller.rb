b8bbcba8b3df3a887d6ed37f2d8c734a2e78252b
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-28 03:04:40 +0000

    [Add]管理者トップページに悩み事の絞り込み検索機能を追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,30 @@
 class Admin::HomesController < Admin::ApplicationController
   def top
-    @problems = Problem.all
+    option = params['option']
+    @problems = search_for(option)
+  end
+
+  private
+
+  def search_for(option)
+    # 全て表示の場合
+    if option == "all"
+      Problem.all
+    # 回答受付中の場合
+    elsif option == "unsolved"
+      @members = Member.where(is_deleted: false)
+      Problem.where(best_answer_id: nil, member_id: @members.ids)
+    # 解決済みの場合
+    elsif option == "solved"
+      @members = Member.all
+      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids)
+    # 回答受付終了の場合
+    elsif option == "finished"
+      @members = Member.where(is_deleted: true)
+      Problem.where(best_answer_id: nil, member_id: @members.ids)
+    # 未選択の場合
+    else
+      Problem.all
+    end
   end
 end
