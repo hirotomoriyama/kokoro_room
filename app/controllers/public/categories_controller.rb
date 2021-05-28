1c98aab9e2e256ed6039d32dfceecd9ab9519ab6
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-28 01:23:16 +0000

    [Add]カテゴリー詳細画面に悩み事の絞り込み検索機能を追加

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -5,5 +5,31 @@ class Public::CategoriesController < Public::ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    option = params['option']
+    @problems = search_for(option)
+  end
+
+  private
+
+  def search_for(option)
+    # 全て表示の場合
+    if option == "all"
+      @category.problems
+    # 回答受付中の場合
+    elsif option == "unsolved"
+      @members = Member.where(is_deleted: false)
+      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category)
+    # 解決済みの場合
+    elsif option == "solved"
+      @members = Member.all
+      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids, category_id: @category)
+    # 回答受付終了の場合
+    elsif option == "finished"
+      @members = Member.where(is_deleted: true)
+      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category)
+    # 未選択の場合
+    else
+      @category.problems
+    end
   end
 end
