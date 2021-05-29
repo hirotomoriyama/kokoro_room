4bdcc5bf53b4965095dee009615bbaf5f2d2d5ee
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-05-29 06:09:20 +0000

    [Add]検索機能に悩み事ステータスの選択肢を追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,19 +4,51 @@ class Public::SearchesController < Public::ApplicationController
     @column = params['column']
     # 検索内容を格納
     @content = params['content']
-    # 上記2つを用いて検索
-    @datas = search_for(@column, @content)
+    # 悩み事ステータスを格納
+    @option = params['option']
+    # 上記3つを用いて検索
+    @datas = search_for(@column, @content, @option)
   end
 
   private
 
-  def search_for(column, content)
+  def search_for(column, content, option)
+    # 検索された悩み事のタイトルを取得
     if column == 'title'
-      # 検索された悩み事のタイトルを取得
-      Problem.where("title LIKE ?", "%#{content}%")
+      # 全て表示の場合
+      if option == "all"
+        Problem.where("title LIKE ?", "%#{content}%")
+      # 回答受付中の場合
+      elsif option == "unsolved"
+        @members = Member.where(is_deleted: false)
+        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+      # 解決済みの場合
+      elsif option == "solved"
+        @members = Member.all
+        Problem.where("title LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids)
+      # 回答受付終了の場合
+      elsif option == "finished"
+        @members = Member.where(is_deleted: true)
+        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+      end
+    # 検索された悩み事の内容を取得
     elsif column == 'body'
-      # 検索された悩み事の内容を取得
-      Problem.where("body LIKE ?", "%#{content}%")
+      # 全て表示の場合
+      if option == "all"
+        Problem.where("body LIKE ?", "%#{content}%")
+      # 回答受付中の場合
+      elsif option == "unsolved"
+        @members = Member.where(is_deleted: false)
+        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+      # 解決済みの場合
+      elsif option == "solved"
+        @members = Member.all
+        Problem.where("body LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids)
+      # 回答受付終了の場合
+      elsif option == "finished"
+        @members = Member.where(is_deleted: true)
+        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+      end
     end
   end
 end
