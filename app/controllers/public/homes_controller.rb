0af8eae4888dcba634ddffca6528c29667fe632e
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-06-10 13:03:12 +0000

    [Add]会員トップページにページャを導入

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -19,16 +19,16 @@ class Public::HomesController < Public::ApplicationController
   def search_for(option)
     # 全て表示の場合
     if option == "all"
-      @member.problems
+      @member.problems.page(params[:page])
     # 回答受付中の場合
     elsif option == "unsolved"
-      @member.problems.where(best_answer_id: nil)
+      @member.problems.where(best_answer_id: nil).page(params[:page])
     # 解決済みの場合
     elsif option == "solved"
-      @member.problems.where.not(best_answer_id: nil)
+      @member.problems.where.not(best_answer_id: nil).page(params[:page])
     # 未選択の場合
     else
-      @member.problems
+      @member.problems.page(params[:page])
     end
   end
 end
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -11,4 +11,7 @@ Kaminari.configure do |config|
   # config.param_name = :page
   # config.max_pages = nil
   # config.params_on_first_page = false
+
+  # ページャを用いて1ページに悩み事を6つまで表示
+  config.default_per_page = 6
 end
