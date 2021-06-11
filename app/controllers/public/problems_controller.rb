53ea7ba3c4e1798dafd93c2991f710a56bbd3f7c
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-06-11 09:28:29 +0000

    [Add]悩み事一覧画面にページャを導入

--- app/controllers/public/problems_controller.rb
+++ app/controllers/public/problems_controller.rb
@@ -57,22 +57,22 @@ class Public::ProblemsController < Public::ApplicationController
   def search_for(option)
     # 全て表示の場合
     if option == "all"
-      Problem.all
+      Problem.page(params[:page])
     # 回答受付中の場合
     elsif option == "unsolved"
       @members = Member.where(is_deleted: false)
-      Problem.where(best_answer_id: nil, member_id: @members.ids)
+      Problem.where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
     # 解決済みの場合
     elsif option == "solved"
       @members = Member.all
-      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids)
+      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids).page(params[:page])
     # 回答受付終了の場合
     elsif option == "finished"
       @members = Member.where(is_deleted: true)
-      Problem.where(best_answer_id: nil, member_id: @members.ids)
+      Problem.where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
     # 未選択の場合
     else
-      Problem.all
+      Problem.page(params[:page])
     end
   end
 end
