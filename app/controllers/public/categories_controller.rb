53f467a0d3d7e918621a4a6c344a4e76b9ad40dd
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-06-12 10:16:44 +0000

    [Add]カテゴリー詳細画面にページャを導入

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -15,22 +15,22 @@ class Public::CategoriesController < Public::ApplicationController
   def search_for(option)
     # 全て表示の場合
     if option == "all"
-      @category.problems
+      @category.problems.page(params[:page])
     # 回答受付中の場合
     elsif option == "unsolved"
       @members = Member.where(is_deleted: false)
-      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category)
+      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category).page(params[:page])
     # 解決済みの場合
     elsif option == "solved"
       @members = Member.all
-      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids, category_id: @category)
+      Problem.where.not(best_answer_id: nil).where(member_id: @members.ids, category_id: @category).page(params[:page])
     # 回答受付終了の場合
     elsif option == "finished"
       @members = Member.where(is_deleted: true)
-      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category)
+      Problem.where(best_answer_id: nil, member_id: @members.ids, category_id: @category).page(params[:page])
     # 未選択の場合
     else
-      @category.problems
+      @category.problems.page(params[:page])
     end
   end
 end
