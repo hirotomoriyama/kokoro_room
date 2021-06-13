a178ddf2dc2300edad0ab4e077dad5127e0a97f0
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-06-13 02:38:29 +0000

    [Add]悩み事検索画面にページャを導入

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -17,37 +17,37 @@ class Public::SearchesController < Public::ApplicationController
     if column == 'title'
       # 全て表示の場合
       if option == "all"
-        Problem.where("title LIKE ?", "%#{content}%")
+        Problem.where("title LIKE ?", "%#{content}%").page(params[:page])
       # 回答受付中の場合
       elsif option == "unsolved"
         @members = Member.where(is_deleted: false)
-        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
       # 解決済みの場合
       elsif option == "solved"
         @members = Member.all
-        Problem.where("title LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids)
+        Problem.where("title LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids).page(params[:page])
       # 回答受付終了の場合
       elsif option == "finished"
         @members = Member.where(is_deleted: true)
-        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+        Problem.where("title LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
       end
     # 検索された悩み事の内容を取得
     elsif column == 'body'
       # 全て表示の場合
       if option == "all"
-        Problem.where("body LIKE ?", "%#{content}%")
+        Problem.where("body LIKE ?", "%#{content}%").page(params[:page])
       # 回答受付中の場合
       elsif option == "unsolved"
         @members = Member.where(is_deleted: false)
-        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
       # 解決済みの場合
       elsif option == "solved"
         @members = Member.all
-        Problem.where("body LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids)
+        Problem.where("body LIKE ?", "%#{content}%").where.not(best_answer_id: nil).where(member_id: @members.ids).page(params[:page])
       # 回答受付終了の場合
       elsif option == "finished"
         @members = Member.where(is_deleted: true)
-        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids)
+        Problem.where("body LIKE ?", "%#{content}%").where(best_answer_id: nil, member_id: @members.ids).page(params[:page])
       end
     end
   end
